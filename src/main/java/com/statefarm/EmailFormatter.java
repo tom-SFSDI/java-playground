979e8ab17a7a73403a7646353251cb135692793e
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-21 14:39:44 -0500

    adjust to remove second main

--- src/main/java/com/statefarm/EmailFormatter.java
+++ src/main/java/com/statefarm/EmailFormatter.java
@@ -1,7 +1,7 @@
 package com.statefarm;
 
 public class EmailFormatter {
-    public static void main(String[] args) {
+    public static void formatted (String[] args) {
      if (args.length < 1) {
          System.out.println("Please specify a name and email.");
      } else if (args.length == 1) {
