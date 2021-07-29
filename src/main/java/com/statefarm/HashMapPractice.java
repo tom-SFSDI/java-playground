cdc9c82f1e6404b72a062a6f026091263f3f5916
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-29 01:51:22 -0500

    hashmap practice

---
+++ src/main/java/com/statefarm/HashMapPractice.java
@@ -0,0 +1,14 @@
+package com.statefarm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HashMapPractice {
+
+    public HashMap<Integer, String> genHashMap() {
+        HashMap<Integer, String> hashedAndMapped = new HashMap<>();
+        hashedAndMapped.put(747, "Airplane");
+        hashedAndMapped.put(10, "Laphroaig");
+        return hashedAndMapped;
+    }
+}
