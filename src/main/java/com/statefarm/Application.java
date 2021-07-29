c3f685203a6cff5940c3e6c0054f76bf645b5642
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-29 01:52:12 -0500

    numerous things.

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -1,16 +1,21 @@
 package com.statefarm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-//        System.out.println("hello world");
-//        EmailFormatter.formatted(args);
-        BankAccount tom = new BankAccount();
-        tom.deposit(100);
-        tom.withdraw(25);
-        tom.balance();
-        tom.isOverDrawn();
-    }
+        System.out.println("hello world");
 
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("My Report.csv");
+        fileNames.add("The Best Deck.pdf");
+        fileNames.forEach((String files) -> {
+            System.out.println(files.replace(" ", "-"));
+
+        });
+    }
 }
 
